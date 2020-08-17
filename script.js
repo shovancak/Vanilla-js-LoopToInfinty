24a9146935ee40153ea40d4e6f90bc28c32db8da
shovancak <s.hovancak@gmail.com>
2020-08-17 22:22:47 +0200

    Fetching data from Unsplash API

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("working");
-alert("WORKING");
+const count = 10;
+const apiKey = "BIZgMIiPodIobg8jzZZDypOfM6Pw3rgvJx4AnVzoW3I";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+const getPicutres = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+getPicutres();
