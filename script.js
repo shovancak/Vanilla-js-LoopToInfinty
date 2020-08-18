f982dc7bdea9d0c4866f83f69935290e23ccabab
shovancak <s.hovancak@gmail.com>
2020-08-18 15:47:59 +0200

    Creating functions for displaying images from API response

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
+const imageContainer = document.getElementById("image-container");
+const loadingSpinner = document.getElementById("loading-spinner");
+
+let imagesArray = [];
+
 const count = 10;
 const apiKey = "BIZgMIiPodIobg8jzZZDypOfM6Pw3rgvJx4AnVzoW3I";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-const getPicutres = async () => {
+const setAttributes = (domElement, attributes) => {
+  for (const key in attributes) {
+    domElement.setAttribute(key, attributes[key]);
+  }
+};
+
+const displayImages = () => {
+  imagesArray.forEach((image) => {
+    const item = document.createElement("a");
+    // item.setAttribute("href", image.links.html);
+    // item.setAttribute("target", "_blank");
+    setAttributes(item, {
+      href: image.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+    // img.setAttribute("src", image.urls.regular);
+    // img.setAttribute("alt", image.alt_description);
+    // img.setAttribute("title", image.alt_description);
+    setAttributes(img, {
+      src: image.urls.regular,
+      alt: image.alt_description,
+      title: image.alt_description,
+    });
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+};
+
+const getImages = async () => {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    imagesArray = await response.json();
+    displayImages();
   } catch (error) {
     console.log(error);
     return error;
   }
 };
 
-getPicutres();
+getImages();
